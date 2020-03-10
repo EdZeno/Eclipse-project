4caf8782c45be8953e285831b25e2ce1b5869202
EdZeno <eduard.kulcsickij.gut@gmail.com>
2020-03-10 16:58:51 +0000

    added a new method

--- FizzBuzz/src/fizzBuzz/FizzBuzz.java
+++ FizzBuzz/src/fizzBuzz/FizzBuzz.java
@@ -1,13 +1,22 @@
 package fizzBuzz;
 
 public class FizzBuzz {
+	
+	public int value(int number) {
+		if (number % 3 == 0) {
+			return 1;
+		} else {
+			return 0;
+		}
+	}
     
+	
 	public String fizzBuzz(int number) {
 		return "Fizz";
 	}
 	
 	public static void main(String[] args) {
-		FizzBuzz fizz = new FizzBuzz();
+//		FizzBuzz fizz = new FizzBuzz();
 	}
 
 }
